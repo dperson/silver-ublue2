5e2fb3695201e7b0124144b15c491ae5a5367503
David Personette <dperson@gmail.com>
2025-05-08 21:37:34 -0400

    chore: Customizations

---
+++ build_files/base/04-packages.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env -S bash
+
+echo "::group:: ===$(basename "$0")==="
+version="${FEDORA_MAJOR_VERSION}"
+set -euxo pipefail
+
+# build list of all packages requested for inclusion
+readarray -t INCLUDED_PACKAGES < <(jq -r "[(.all.include | \
+      (select(.all != null).all)[]), \
+      (select(.\"$version\" != null).\"$version\".include | \
+      (select(.all != null).all)[])] | sort | unique[]" /tmp/packages.json)
+
+# Install Packages
+if [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 ]]; then
+  dnf5 -y install "${INCLUDED_PACKAGES[@]}"
+else
+  echo "No packages to install."
+fi
+
+# build list of all packages requested for exclusion
+readarray -t EXCLUDED_PACKAGES < <(jq -r "[(.all.exclude | \
+      (select(.all != null).all)[]), \
+      (select(.\"$version\" != null).\"$version\".exclude | \
+      (select(.all != null).all)[])] | sort | unique[]" /tmp/packages.json)
+
+if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
+  readarray -t EXCLUDED_PACKAGES < \
+        <(rpm -qa --queryformat='%{NAME}\n' "${EXCLUDED_PACKAGES[@]}")
+fi
+
+# remove any excluded packages which are still present on image
+if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
+  dnf5 -y remove "${EXCLUDED_PACKAGES[@]}"
+else
+  echo "No packages to remove."
+fi
+
+# Add workaround for xdg-desktop-portal
+dnf5 -y upgrade --enablerepo=updates-testing --refresh \
+      --advisory=FEDORA-2025-c358833c5d
+
+echo "::endgroup::"
